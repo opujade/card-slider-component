931f49175d9efeedcb71fce588cb7f50d23acffe
Oriol Pujade Busqueta <uripujade@gmail.com>
2024-04-17 13:32:55 +0200

    exercise 3 maquetacio

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,21 @@ function App() {
       description:
         'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però aniràs notant una millora ràpidament.',
       bgColor: '#4da2a9',
-      image: 'time_management.svg',
+      image: 'src/assets/time_managment.svg',
     },
     {
       title: 'Programa projectes propis',
       description:
         'Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.',
       bgColor: '#d3d4d9',
-      image: 'programming.svg',
+      image: 'src/assets/programming.svg',
     },
     {
       title: 'Procura descansar',
       description:
         "Descansar bé i desconnectar són vitals. D'aquesta manera reduïràs l'estrés i l'ansietat. Milloraràs la concentració i consolidaràs el teu aprenentatge.",
       bgColor: '#ffd167',
-      image: 'meditation.svg',
+      image: 'src/assets/meditation.svg',
     },
   ];
 
@@ -34,7 +34,16 @@ function App() {
 
   return (
     <>
-      <Card currentCardData={tutorialData[step]} nextStep={nextStep} prevStep={prevStep}></Card>
+      <div className='flex justify-center items-center h-screen bg-slate-50 font-sans'>
+        <Card
+          currentCardData={tutorialData[step]}
+          nextStep={nextStep}
+          prevStep={prevStep}
+        >
+          <h3 className='font-bold text-xl pt-8 px-5'>{tutorialData[step].title}</h3>
+          <p className='font-thin py-3 ps-5 pe-10'>{tutorialData[step].description}</p>
+        </Card>
+      </div>
     </>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,94 @@
 import React from 'react';
 
-export const Card = ({ currentCardData, nextStep, prevStep }) => {
+const Button = ({ chooseStep, value, disabled, btnColor }) => {
+  return (
+    <button
+      className={
+        'transition border-solid border-gray-800 font-bold border py-3 m-1 px-5 rounded-full ' +
+        btnColor.bg +
+        ' ' +
+        btnColor.color +
+        ' ' +
+        btnColor.hover
+      }
+      onClick={chooseStep}
+      type="button"
+      disabled={disabled}
+    >
+      {value}
+    </button>
+  );
+};
+
+export const Card = ({
+  children,
+  currentCardData,
+  nextStep,
+  prevStep,
+  ...props
+}) => {
+  let disabled = [false, false];
+  let btnColor = [
+    {
+      bg: 'bg-white',
+      color: 'text-black',
+      hover: 'hover:bg-black hover:text-white',
+    },
+    {
+      bg: 'bg-black',
+      color: 'text-white',
+      hover: 'hover:bg-white hover:text-black',
+    },
+  ];
+
+  if (currentCardData.title == 'Dedica moltes hores') {
+    disabled[0] = true;
+    btnColor[0] = {
+      bg: 'bg-gray-300 border-gray-300',
+      color: 'text-black',
+    };
+  }
+  if (currentCardData.title == 'Procura descansar') {
+    disabled[1] = true;
+    btnColor[1] = {
+      bg: 'bg-gray-300 border-gray-300',
+      color: 'text-black',
+    };
+  }
+
   return (
     <>
-      <h2>{currentCardData.title}</h2>
-      <p>{currentCardData.description}</p>
-      <button onClick={nextStep} type='button'>Next Step</button>
-      <button onClick={prevStep} type='button'>Prev Step</button>
+      <div className="max-w-sm bg-white rounded-3xl overflow-hidden h-5/6 relative">
+        <div
+          className={
+            'transition h-3/5 bg-[' +
+            currentCardData.bgColor +
+            '] m-0 flex items-center justify-center'
+          }
+        >
+          <img
+            className="transition p-5"
+            src={currentCardData.image}
+            alt={'Imatge de "' + currentCardData.title + '"'}
+          ></img>
+        </div>
+        <div>{children}</div>
+        <div className="pt-5 absolute end-0 bottom-0 my-6 me-3">
+          <Button
+            chooseStep={prevStep}
+            value={'<'}
+            disabled={disabled[0]}
+            btnColor={btnColor[0]}
+          ></Button>
+
+          <Button
+            chooseStep={nextStep}
+            value={'>'}
+            disabled={disabled[1]}
+            btnColor={btnColor[1]}
+          ></Button>
+        </div>
+      </div>
     </>
   );
 };
