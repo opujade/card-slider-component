1ded4b846a41c984bbe70caf4aa5ef50bd43a2ac
Oriol Pujade Busqueta <uripujade@gmail.com>
2024-04-18 13:56:01 +0200

    added dots animation

--- src/components/Indicator.jsx
+++ src/components/Indicator.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 export const Indicator = ({ amountOfSteps, step, index }) => {
-  let width = step == index ? 'w-6' : 'w-3';
+  let width = step == index ? 'w-6 opacity-80' : 'w-3 opacity-50';
 
   return (
     <>
       <li
-        className={`${width} h-3 transition bg-black m-1 rounded-full hover:opacity-100 cursor-pointer`}
+        className={`${width} h-3 duration-500 bg-black m-1 rounded-full hover:opacity-100 cursor-pointer`}
       ></li>
     </>
   );
